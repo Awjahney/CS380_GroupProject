55d8df32cad4272597670f578ec0ec537decc84c
FisherBR6 <fisherbachmanrhodes@gmail.com>
2024-11-22 10:22:11 -0800

    Gui Driver File

---
+++ guiDriver.java
@@ -0,0 +1,8 @@
+public class guiDriver {
+    public static void main(String[] args) {
+        //build login gui screen
+        GUI_3 gui3 = new GUI_3();
+        gui3.buildGuiPanel();
+        
+    }
+}
